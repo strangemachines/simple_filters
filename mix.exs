f793519dfb3531792ee5197db533392d73f97b56
nomorepanic <nomorepanic@strangemachines.dev>
2024-03-10 22:32:44 +0100

    chore/simple_filters: version 1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SimpleFilters.MixProject do
   def project do
     [
       app: :simple_filters,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
